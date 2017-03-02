5dd4a6c99cf8f212bfea723bfe46a0f00bd34026
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-02 19:11:01 +0530

    original code

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 var student = {
-  name:"",
-  type:"student"
+  name: "",
+  type: "student"
 };
 
-document.addEventListener('DOMContentLoaded',contentLoader);
-function contentLoader(event){
-  document.getElementById('name').addEventListener("Keyup", keyUp);
+document.addEventListener('DOMContentLoaded', contentLoaded);
 
+function contentLoaded(event) {
+  document.getElementById('name').addEventListener("keyup", keyUp);
 }
-function keyUp(event){
-  calculateOutput();
-}
-function calculateOutput(){
-  student.name=document.getElementById('name').value;
 
-  var total = 0;
-  for(var i=0; i < student.name.length; i++ ){
-    total += student.name.charCodeAt(i);
+function keyUp(event) {
+  calculateNumericOutput();
+}
+
+function calculateNumericOutput() {
+  student.name = document.getElementById('name').value;
+
+  var totalNameValue = 0;
+  for (var i = 0; i < student.name.length; i++) {
+    totalNameValue += student.name.charCodeAt(i);
   }
-  var output="Total Character in name" + total;
+
+  // Insert result into page
+  var output = "Total Numeric value of person's name is " + totalNameValue;
   document.getElementById('output').innerText = output;
 }
