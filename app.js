8f96a088e5b2b353cc2c63fbbfd7b11a9a345483
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-03 15:13:24 +0530

    models added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('mainController', function($scope){
-$scope.name = "Devendra";
+$scope.name = "";
+$scope.totalValue = 0;
 $scope.sayYes = function(){
   return "Hello!";
 }
