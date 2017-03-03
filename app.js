c0e6e158e3eb433eb81b1d4755db22b28221e43b
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-03 12:21:31 +0530

    angular start

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-var student = {
-  name:"",
-  type:"student"
-};
+(function (){
+angular.module('myApp',[])
 
-document.addEventListener('DOMContentLoaded',contentLoader);
-function contentLoader(event){
-  document.getElementById('name').addEventListener("Keyup", keyUp);
+.controller('mainController', function(){
+  
 
-}
-function keyUp(event){
-  calculateOutput();
-}
-function calculateOutput(){
-  student.name=document.getElementById('name').value;
-
-  var total = 0;
-  for(var i=0; i < student.name.length; i++ ){
-    total += student.name.charCodeAt(i);
-  }
-  var output="Total Character in name" + total;
-  document.getElementById('output').innerText = output;
-}
+});
+})();
