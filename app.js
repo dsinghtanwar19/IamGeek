b4682bc48a6e2f965b25876833a775bfd33ed66d
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-03 15:28:03 +0530

    calculate

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('mainController', function($scope){
 $scope.name = "";
 $scope.totalValue = 0;
-$scope.sayYes = function(){
-  return "Hello!";
+$scope.showValue = function(){
+  var totalNoValue = calculateValue($scope.name);
+  $scope.totalValue = totalNoValue;
+}
+function calculateValue(string){
+  var stringValue = 0;
+  for (var i = 0 ; i < string.length; i++){
+    stringValue += string.charCodeAt(i);
+  }
+  return stringValue;
 }
 });
