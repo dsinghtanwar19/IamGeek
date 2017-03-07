b889a9ab870df937f68dd45a284e16c970781425
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-07 19:13:07 +0530

    FEFEF

--- app.js
+++ app.js
@@ -2,47 +2,13 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('mainController', function($scope, $filter){
-$scope.name = "Devendra";
-$scope.stateOfBeing = "hungry";
-$scope.cookieCost = .45;
-/*$scope.totalValue = 0;
-$scope.showValue = function(){
-  var totalNoValue = calculateValue($scope.name);
-  $scope.totalValue = totalNoValue;
-}
-function calculateValue(string){
-  var stringValue = 0;
-  for (var i = 0 ; i < string.length; i++){
-    stringValue += string.charCodeAt(i);
-  }
-  return stringValue;
-}*/
-$scope.upper = function (){
-  var upCase = $filter('uppercase');
-  $scope.name= upCase($scope.name);
-}
- function dictateMe(name, jhohn , dev){
-   return "hurray:";
- }
- console.log(dictateMe());
 
- $scope.sayYes = function(){
-   var msg = "Dev likes pizza";
-   var output = $filter('uppercase')(msg);
-   return output;
- }
- $scope.sayLovesMessage = function () {
-    var msg = "Dev likes pizza";
-    msg = $filter('lovesFilter')(msg);
-    return msg;
-  };
- $scope.feedDev = function(){
-    $scope.stateOfBeing = "fed";
- }
 
- $scope.showNoOfWatchers = function(){
+ $scope.showNumberOfWatchers = function () {
    console.log($scope);
- }
+ };
+
+
 });
 
 myApp.filter('lovesFilter', function(){
