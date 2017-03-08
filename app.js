33d71815df104aca82b421b0c827814d4b4c08cc
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-08 13:12:24 +0530

    FSAFSAFDSFSADFSAFSAF

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ $scope.counter = 0;
 
 
  $scope.upCounter = function(){
-   setTimeout(function(){
-     $scope.$apply(function(){
-       $scope.counter++;
-       console.log("Counter incremented!");
-     });
+   $timeout(function(){
+     $scope.counter++;
+     console.log("Counter incremented!");
    }, 2000);
 
  };
