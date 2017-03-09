731e444fa58d0c4af190d0ce597a0351eaefc7b7
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-09 12:36:58 +0530

    WQFDA

--- filter.js
+++ filter.js
@@ -1,8 +1,11 @@
 var numberArray = [1,2,3,4,5,6];
 console.log(numberArray);
 
-var filteredNumberArray = numberArray.filter(function(value){
-  return value < 5;
-})
+function below5Filter(value){
+    return value < 5;
+
+}
+
+var filteredNumberArray = numberArray.filter(below5Filter);
 
 console.log(filteredNumberArray);
