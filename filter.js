dfdd60d568e3e47c6c4c61a47ae926a66e413cfd
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-09 12:46:39 +0530

    FSADF FSDV

--- filter.js
+++ filter.js
@@ -9,3 +9,15 @@ function below5Filter(value){
 var filteredNumberArray = numberArray.filter(below5Filter);
 
 console.log(filteredNumberArray);
+var shoppingList = [
+ "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+];
+console.log("item", shoppingList);
+
+var searchValue = "Bismol";
+function containsFilter(value){
+  return value.indexOf(searchValue) !== -1;
+}
+
+var filterList = shoppingList.filter(containsFilter);
+console.log(filterList);
