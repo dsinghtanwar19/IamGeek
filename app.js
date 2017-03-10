102a12137c120dc2cf87c7adaf7ff4c6bda06722
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-10 12:48:24 +0530

    FF

--- app.js
+++ app.js
@@ -10,4 +10,16 @@ $scope.pc.parentValue=1;
 myApp.controller('childController', function($scope, $filter, $timeout){
 console.log("$scope.parentValue:", $scope.parentValue);
 console.log("child $scope : ", $scope);
+$scope.parentValue = 5;
+  console.log("*** CHANGED: $scope.parentValue = 5 ***");
+   console.log("$scope.parentValue: ", $scope.parentValue);
+   console.log($scope);
+   console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+  $scope.pc.parentValue = 5;
+   console.log("** CHANGED: $scope.pc.parentValue = 5; ***");
+   console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+   console.log("$scope: ", $scope);
+
+   console.log("$scope.$parent.parentValue: ", $scope.$parent.parentValue);
+}
 });
