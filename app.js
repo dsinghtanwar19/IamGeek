1c7ac0efca8ffdc4453ea76ecce4d1ae3842f89c
Devendra Singh Tanwar <DevendraSingh.Tanwar@pb.com>
2017-03-10 13:13:23 +0530

    FWEFEFWEGWGWGW

--- app.js
+++ app.js
@@ -8,18 +8,14 @@ $scope.pc.parentValue=1;
 
 });
 myApp.controller('childController', function($scope, $filter, $timeout){
-console.log("$scope.parentValue:", $scope.parentValue);
-console.log("child $scope : ", $scope);
-$scope.parentValue = 5;
-  console.log("*** CHANGED: $scope.parentValue = 5 ***");
-   console.log("$scope.parentValue: ", $scope.parentValue);
-   console.log($scope);
-   console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
-  $scope.pc.parentValue = 5;
-   console.log("** CHANGED: $scope.pc.parentValue = 5; ***");
-   console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
-   console.log("$done for: ", $scope);
 
-   console.log("$scope.$parent.parentValue: ", $scope.$parent.parentValue);
-
+});
+myApp.controller('parentController2', function(){
+var parent = this;
+parent.value = 1;
+});
+myApp.controller('childController2', function($scope, $filter, $timeout){
+var child = this;
+child.value = 5;
+console.log("test", $scope);
 });
